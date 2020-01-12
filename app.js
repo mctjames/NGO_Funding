60fc557d3c7d67d153cf952dff5de7f6957291d3
jmctagga <jmctagga@sfu.ca>
2020-01-11 18:54:35 -0800

    template expanded

--- app.js
+++ app.js
@@ -20,8 +20,39 @@ app.use(express.static(__dirname+ '/public'));
 // Home page
 app.get('/', function(req, res){
 	res.render('pages/home');
-	//res.send('Hello world');
 });
+
+app.get('/about', function(req, res ){
+	res.render('pages/about')
+})
+
+app.get('/causes', function(req, res ){
+	res.render('pages/causes')
+})
+
+app.get('/contact', function(req, res ){
+	res.render('pages/contact')
+})
+
+app.get('/elements', function(req, res ){
+	res.render('pages/elements')
+})
+
+app.get('/index', function(req, res ){
+	res.render('pages/index')
+})
+
+app.get('/news', function(req, res ){
+	res.render('pages/news')
+})
+
+app.get('/portfolio', function(req, res ){
+	res.render('pages/portfolio')
+})
+
+app.get('/single-cause', function(req, res ){
+	res.render('pages/single-cause')
+})
 
 
 
