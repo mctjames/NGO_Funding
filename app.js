be74c787863a0a8f6473433000d5a1b85daea547
jmctagga <jmctagga@sfu.ca>
2020-01-11 16:53:15 -0800

    nodejs template updated

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
 
+// const bodyParser    =   require('body-parser')
+// const session       =   require('express-session')
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(__dirname+ '/public'));
+
+
+
+
+
+
+
+// Home page
 app.get('/', function(req, res){
-	res.send('Hello world');
+	res.render('pages/home');
+	//res.send('Hello world');
 });
 
+
+
+
+
+// start server
 app.listen(5000, function(){
 	console.log('server started on port 5000');
 })
+
+
+
+
